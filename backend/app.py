8ea5011b21547620bbf22e2b5b8df51ae05ed919
Abhishek <abhitiku2003@gmail.com>
2024-04-24 19:46:30 +0530

    minor

--- backend/app.py
+++ backend/app.py
@@ -55,6 +55,3 @@ def analyze():
         return jsonify({"error": f"Missing data for feature: {str(e)}"}), 400
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-
-if __name__ == '__main__':
-    app.run(debug=True)
